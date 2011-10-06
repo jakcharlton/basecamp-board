07d2c2312d673ceb99e0c12d125f80d56f61c781
Jak Charlton <jak@thinkddd.com>
2011-10-06 18:34:42 +1100

    tidy up

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,10 +3,10 @@ class TodoController < ApplicationController
 
   def index
   	Basecamp.establish_connection!('thefarm1.basecamphq.com', '', '', true)
-	all_todos = BasecampHelper.todo_items_on_project 8154818
-	@todo = all_todos.select{|i| i.status == :to_do }
-	@in_progress = all_todos.select{|i| i.status == :in_progress }
-	@done = all_todos.select{|i| i.status == :done }
+    all_todos = BasecampHelper.todo_items_on_project 8154818
+    @todo = all_todos.select{|todo| todo.status == :to_do }
+    @in_progress = all_todos.select{|todo| todo.status == :in_progress }
+    @done = all_todos.select{|todo| todo.status == :done }
   end
 
 end
