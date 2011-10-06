ee196f1d29d8bb2a663bed8e0aab50ba4feb22ae
Jak Charlton <jak@thinkddd.com>
2011-10-06 20:11:28 +1100

    Refactor to properly trap errors from BC

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,16 +4,17 @@ class TodoController < ApplicationController
   def index
     begin
       user = session[:user]
-      p user
   	  Basecamp.establish_connection!(user.project_url, user.username, user.password, true)
-	  rescue ActiveResource::UnauthorizedAccess
-	    redirect_to :controller => "users", :action => "index"
-    end
+
   
-    all_todos = BasecampHelper.todo_items_on_project 8154818
-    @todo = all_todos.select{|todo| todo.status == :to_do }
-    @in_progress = all_todos.select{|todo| todo.status == :in_progress }
-    @done = all_todos.select{|todo| todo.status == :done }
+      all_todos = BasecampHelper.todo_items_on_project 8154818
+      @todo = all_todos.select{|todo| todo.status == :to_do }
+      @in_progress = all_todos.select{|todo| todo.status == :in_progress }
+      @done = all_todos.select{|todo| todo.status == :done }
+    rescue
+      redirect_to :controller => "users", :action => "index"
+    end
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     
     session[:user] = user
 
-    redirect_to :controller => "todo", :action => "index"
+    redirect_to :controller => "todo"
   end
 
 end
